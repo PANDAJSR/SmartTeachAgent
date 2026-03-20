fafcbd06be53fcea05958d9460fd8829633d0c3a
JSR <pandajsr72@gmail.com>
2026-03-20 20:38:14 +0800

    commit 一下

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  const devServerUrl = process.env.VITE_DEV_SERVER_URL;
+  if (devServerUrl) {
+    win.loadURL(devServerUrl);
+  } else {
+    win.loadFile(path.join(__dirname, "dist", "index.html"));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="app">
+      <h1>SmartTeachAgent</h1>
+      <p>Electron + React + Vite</p>
+    </main>
+  );
+}
+
+export default App;
